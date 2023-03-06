4a541542db205bf22fb3936108d7c7010304058f
aalbrech <Andreas.Albrecht@edvschule-plattling.de>
2023-03-06 09:49:21 +0100

    Toolbar

---
+++ DnD_Map_Maker_3D/Assets/Scripts/Toolbar.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Toolbar : MonoBehaviour
+{
+    public void onErhoehen()
+    {}
+    public void onErniedrigen()
+    {}
+}
