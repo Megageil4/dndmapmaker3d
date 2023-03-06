c498a41a1bf6b829811ee68c090d658bfeb2c1af
aalbrech <Andreas.Albrecht@edvschule-plattling.de>
2023-03-06 15:11:30 +0100

    Map loading begonnen

--- DnD_Map_Maker_3D/Assets/Scripts/Startup.cs
+++ DnD_Map_Maker_3D/Assets/Scripts/Startup.cs
@@ -4,15 +4,17 @@ using UnityEngine;
 
 public class Startup : MonoBehaviour
 {
+    public GameObject MenuController;
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if (ServerKomm.existsMap())
+        {
+            ServerKomm.fetchMap();
+        }
+        else
+        {
+            MenuController.GetComponent<MenuBarController>().MakeGridSizePopUpVisible();
+        }
     }
 }
