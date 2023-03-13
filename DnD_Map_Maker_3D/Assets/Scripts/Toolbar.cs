1edf097279b9beb65cf520e9e77ff2b4ccf94c45
dlekomze <Daniel.Lekomzew@edvschule-plattling.de>
2023-03-13 13:28:34 +0100

    Updated Icons

--- DnD_Map_Maker_3D/Assets/Scripts/Toolbar.cs
+++ DnD_Map_Maker_3D/Assets/Scripts/Toolbar.cs
@@ -1,10 +1,13 @@
+using System.Collections.Generic;
 using DefaultNamespace;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Toolbar : MonoBehaviour
 {
     public PlaneSpawner PlaneSpawner;
     public static IMapTool MapTool;
+    public List<Button> buttons;
     public void onErhoehen()
     {
         MapTool = new Extrude(PlaneSpawner);
