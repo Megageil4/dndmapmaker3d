1f85f9de72ef6314ae160d9b5474291eb9f7796e
dlekomze <Daniel.Lekomzew@edvschule-plattling.de>
2023-04-17 10:04:36 +0200

    Merged Websoket branch

--- DnD_Map_Maker_3D/Assets/Scripts/DataContainer.cs
+++ DnD_Map_Maker_3D/Assets/Scripts/DataContainer.cs
@@ -4,6 +4,11 @@ public class DataContainer
     public static string ServerIP { get; set; }
     public static IDnDConnection Conn { get; private set; }
 
+    static DataContainer()
+    {
+        ServerIP = "10.0.207.3";
+    }
+    
     public static void CreateConn(IDnDConnection con)
     {
         Conn = con;
