d095e2316d0e272d4b9358fe2876c18c1ae4a0b8
dlekomze <Daniel.Lekomzew@edvschule-plattling.de>
2023-04-17 10:25:28 +0200

    Added Gameobject Dictonary and Timestamp for JK_GameObjects

--- DnD_Map_Maker_3D/Assets/Scripts/ConnStuff/JK_GameObject.cs
+++ DnD_Map_Maker_3D/Assets/Scripts/ConnStuff/JK_GameObject.cs
@@ -1,21 +1,61 @@
 ﻿using System;
+using Unity.VisualScripting;
 using UnityEngine;
+using Update = UnityEngine.PlayerLoop.Update;
 
 namespace ConnStuff
 {
     public class JK_GameObject
     {
-        public float[] pos3 { get; set; }
-        public float[] rot3 { get; set; }
-        public float[] scale3 { get; set; }
-        public Guid Guid { get; set; }
+        private float[] _pos3;
+        private float[] _rot3;
+        private float[] _scale3;
 
+        public float[] pos3
+        {
+            get => _pos3;
+            set
+            {
+                Update();
+                _pos3 = value;
+            }
+        }
+
+        public float[] rot3
+        {
+            get => _rot3;
+            set
+            {
+                Update();
+                _rot3 = value;
+            }
+        }
+
+        public float[] scale3
+        {
+            get => _scale3;
+            set
+            {
+                Update();
+                _scale3 = value;
+            }
+        }
+
+        public Guid Guid { get; private set; }
+        public DateTime LastChanged { get; private set; }
+
+        private void Update()
+        {
+            LastChanged = DateTime.Now;
+        }
+        
         public JK_GameObject(float[] pos3, float[] rot3, float[] scale3, Guid guid)
         {
             this.pos3 = pos3;
             this.rot3 = rot3;
             this.scale3 = scale3;
             this.Guid = guid;
+            LastChanged = DateTime.Now;
         }
 
         public JK_GameObject(GameObject go)
@@ -30,6 +70,8 @@ namespace ConnStuff
             this.scale3 = new []{localScale[0], localScale[1], localScale[2]};
             
             this.Guid = Guid.NewGuid();
+
+            LastChanged = DateTime.Now;
         }
     }
 }
