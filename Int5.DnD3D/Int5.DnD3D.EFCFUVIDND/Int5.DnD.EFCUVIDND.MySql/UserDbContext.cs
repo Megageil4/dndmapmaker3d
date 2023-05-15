999c9b43d6c7ec6bbf20edaf820172fed4634ebf
jkaufman <Johann.Kaufmann@edvschule-plattling.de>
2023-05-15 08:56:06 +0200

    EFCore für Userverwaltungsdatenbank angelegt

---
+++ Int5.DnD3D/Int5.DnD3D.EFCFUVIDND/Int5.DnD.EFCUVIDND.MySql/UserDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Int5.DnD3D.EFCUVIDND.Entity;
+using Microsoft.EntityFrameworkCore;
+
+namespace Int5.DnD.EFCUVIDND.MySql
+{
+    public class UserDbContext : DbContext
+    {
+		#region Properties
+		public DbSet<User> TeamSet => Set<User>();
+		#endregion
+
+
+		#region Konstruktoren
+		public UserDbContext() : base() { }
+		public UserDbContext(DbContextOptions options) : base(options) { }
+		#endregion
+
+		#region Methoden
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			modelBuilder.HasDefaultSchema("DnD");
+			modelBuilder.Entity<User>().ToTable("User");
+			
+			
+		}
+
+
+		#endregion
+	}
+}
---
+++ Int5.DnD3D/Int5.DnD3D.EFCFUVIDND/Int5.DnD3D.EFCUVIDND.Entity/User.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Int5.DnD3D.EFCUVIDND.Entity
+{
+    public class User
+    {
+		public int Id { get; set; }
+		[StringLength(20)]
+		public string Username { get; set; }
+
+		#region Konstruktoren
+		public User(string username)
+		{
+			Username = username;
+		}
+		public User()  : this(String.Empty) { }
+		#endregion
+	}
+}
